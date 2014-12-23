c3e4cfb9e5a790542b78e32df4e1e2818d032007
Chester <cwyark@gmail.com>
2014-12-24 01:45:48 +0800

    add gcc in linux x86

---
+++ build/linux/x86/lib/gcc/msp430/4.7.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --target=msp430 --enable-languages=c,c++ --prefix=/home/yusheng/project/msp430-gcc-toochain/build/linux/x86";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
